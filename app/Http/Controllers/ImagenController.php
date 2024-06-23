158d0a4ec3f6520b7e7911615a5b8c5d6d836c6c
AokamiX <alejandrojesus.cerpa@gmail.com>
2024-06-23 14:55:24 -0400

    Proyecto

---
+++ app/Http/Controllers/ImagenController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+
+class ImagenController extends Controller
+{
+    //
+    public function store(Request $request){
+        
+        $imagen = $request->file('file');
+
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+        $imagenServidor = Image::make($imagen);
+        $imagenServidor->fit(1000, 1000);
+
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        if(!file_exists(public_path('uploads'))){
+            mkdir(public_path('uploads'));
+        }
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen' => $nombreImagen]);
+    }
+}
